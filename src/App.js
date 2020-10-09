8456e9cc8a0e844f1a720bf30eaecd959cda4f9e
Liz Martinez <40329316+elizabethmv@users.noreply.github.com>
2020-10-09 15:00:24 -0500

    Add react-paypal-js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PayPalScriptProvider options={{ "client-id": "sb" }}>
+      <PayPalButtons style={{ layout: "horizontal" }} />
+    </PayPalScriptProvider>
   );
 }
-
-export default App;
